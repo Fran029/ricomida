8354de2050897747b35bd83a0c5663132d8be0e8
Francisca Robles <flanna.jpg@gmail.com>
2024-03-13 20:54:13 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$( "#enviarCorreo" ).on( "click", function() {
+    alert( "El correo fue enviado correctamente" );
+    
+} );
+
+$( "h4" ).on( "click", function() {
+    alert("color cambia")
+}) ;
+
+function prueba( ){
+
+}
